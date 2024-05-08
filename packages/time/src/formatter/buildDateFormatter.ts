5e520ec857016db6096e7ff46c49deb1d703fcd7
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2024-05-08 11:22:30 -0400

    docs: Add JSDocs function explainers

--- packages/time/src/formatter/buildDateFormatter.ts
+++ packages/time/src/formatter/buildDateFormatter.ts
@@ -1,10 +1,55 @@
+import { getDefaultLocale } from "../utils/dateDefaults";
 import { extractLocaleOptions } from "./extractLocaleOptions";
 import type { IDateFormatterBuildParams } from "./shared";
 
+/**
+ * @typedef {Object} IDateFormatterOptions
+ * @property {string} [localeMatcher]
+ * @property {string} [calendar]
+ * @property {string} [numberingSystem]
+ * @property {boolean} [hour12]
+ * @property {string} [hourCycle]
+ * @property {string} [timeZone]
+ * @property {string} [formatMatcher]
+ * @property {string} [weekday]
+ * @property {string} [era]
+ * @property {string} [year]
+ * @property {string} [month]
+ * @property {string} [day]
+ * @property {string} [dateStyle]
+ * 
+ * @typedef {string | Intl.Locale | string[] | Intl.Locale[]} Locale
+ * 
+ * @typedef {Object} IDateFormatterBuildParams
+ * @property {Locale} [locale]
+ * @property {string | IDateFormatterOptions} [options]
+ */
+
+/**
+ * Function: buildDateFormatter
+ * This function is used to build a date only formatter, using the Intl.DateTimeFormat constructor.
+ * The 'locale' named parameter can be a string, an Intl.Locale object, or an array of strings 
+ * or Intl.Locale objects. This is defaulted to the user's browser locale.
+ * 
+ * The 'options' named parameter can be a string or an object.
+ * 
+ * If 'options' is a string, it will be used as the 'dateStyle' option.
+ *  See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#datestyle as reference.
+ * 
+ * If 'options' is an object, it will use the Intl.DateTimeFormat 'options'. 
+ *  See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options as reference.
+ * 
+ * You use the 'options' object for finer level of control over the date formatting.
+ * When using UTC date strings, it is suggested that you use the 'options' object
+ * to set the 'timeZone' when building the formatter. The 'timeZone' is defaulted
+ * to the user's browser timezone.
+ * @param {IDateFormatterBuildParams} [param0]
+ * @returns Intl.DateTimeFormat
+ */
 export function buildDateFormatter({
-  locale, 
+  locale = getDefaultLocale(), 
   options
-}: IDateFormatterBuildParams): Intl.DateTimeFormat {
+}: IDateFormatterBuildParams = {}): Intl.DateTimeFormat {
   const opts = (typeof options === 'string') ? { dateStyle: options } : options ?? {};
   const {formatOptions = {}, ...localeOptions} = extractLocaleOptions(opts);
   const { dateStyle, ...rest } = formatOptions;
--- packages/time/src/formatter/extractLocaleOptions.ts
+++ packages/time/src/formatter/extractLocaleOptions.ts
@@ -1,7 +1,79 @@
+import { getDefaultCalendar, getDefaultTimeZone } from "../utils/dateDefaults";
 import type { IDateFormatterOptions, IDateTimeFormatterOptions, ITimeFormatterOptions  } from "./shared";
 
+/**
+ * @typedef {Object} IDateFormatterOptions
+ * @property {string} [localeMatcher]
+ * @property {string} [calendar]
+ * @property {string} [numberingSystem]
+ * @property {boolean} [hour12]
+ * @property {string} [hourCycle]
+ * @property {string} [timeZone]
+ * @property {string} [formatMatcher]
+ * @property {string} [weekday]
+ * @property {string} [era]
+ * @property {string} [year]
+ * @property {string} [month]
+ * @property {string} [day]
+ * @property {string} [dateStyle]
+ * 
+ * @typedef {Object} ITimeFormatterOptions
+ * @property {string} [localeMatcher]
+ * @property {string} [calendar]
+ * @property {string} [numberingSystem]
+ * @property {boolean} [hour12]
+ * @property {string} [hourCycle]
+ * @property {string} [timeZone]
+ * @property {string} [formatMatcher]
+ * @property {string} [dayPeriod]
+ * @property {string} [hour]
+ * @property {string} [minute]
+ * @property {string} [second]
+ * @property {number} [fractionalSecondDigits]
+ * @property {string} [timeZoneName]
+ * @property {string} [timeStyle]
+ * 
+ * @typedef {Object} IDateTimeFormatterOptions
+ * @property {string} [localeMatcher]
+ * @property {string} [calendar]
+ * @property {string} [numberingSystem]
+ * @property {boolean} [hour12]
+ * @property {string} [hourCycle]
+ * @property {string} [timeZone]
+ * @property {string} [formatMatcher]
+ * @property {string} [weekday]
+ * @property {string} [era]
+ * @property {string} [year]
+ * @property {string} [month]
+ * @property {string} [day]
+ * @property {string} [dateStyle]
+ * @property {string} [dayPeriod]
+ * @property {string} [hour]
+ * @property {string} [minute]
+ * @property {string} [second]
+ * @property {number} [fractionalSecondDigits]
+ * @property {string} [timeZoneName]
+ * @property {string} [timeStyle]
+ */
+
+ /** 
+ * Function: extractLocaleOptions
+ * This function is used to extract the locale options from the 'options' parameter of the various
+ * formatter 'build' functions.
+ * 
+ * If 'calender' or 'timeZone' are not provided, the default values provided by the
+ * Intl.DateTimeFormat().resolvedOptions() are used.
+ * @param {IDateFormatterOptions | IDateTimeFormatterOptions | ITimeFormatterOptions} param0 
+ * @returns 
+ */
 export function extractLocaleOptions({
-  localeMatcher, calendar, numberingSystem, hour12, hourCycle, timeZone, ...formatOptions
+  localeMatcher, 
+  calendar = getDefaultCalendar(), 
+  numberingSystem, 
+  hour12, 
+  hourCycle, 
+  timeZone = getDefaultTimeZone(), 
+  ...formatOptions
 }: IDateFormatterOptions | IDateTimeFormatterOptions | ITimeFormatterOptions) {
   return {
     localeMatcher, calendar, numberingSystem, hour12, hourCycle, timeZone, formatOptions
