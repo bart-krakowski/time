0b9bdeb6ee4818791da704c8ff6c0c914d03cde9
Bart Krakowski <bartlomiej@krakowski.dev>
2024-06-02 23:39:32 +0200

    test: useCalendar tests

--- packages/react-time/src/tests/useCalendar.test.ts
+++ packages/react-time/src/tests/useCalendar.test.ts
@@ -1,7 +1,7 @@
-import { Temporal } from '@js-temporal/polyfill';
-import { describe, expect, test } from 'vitest';
-import { act, renderHook } from '@testing-library/react';
-import { useCalendar } from '../useCalendar';
+import { Temporal } from '@js-temporal/polyfill'
+import { describe, expect, test } from 'vitest'
+import { act, renderHook } from '@testing-library/react'
+import { useCalendar } from '../useCalendar'
 
 describe('useCalendar', () => {
   const events = [
@@ -17,83 +17,108 @@ describe('useCalendar', () => {
       endDate: Temporal.PlainDateTime.from('2024-06-02T16:00:00'),
       title: 'Event 2',
     },
-  ];
+  ]
 
-  const mockEvent = {} as React.MouseEvent<HTMLButtonElement, MouseEvent>;
+  const mockEvent = {} as React.MouseEvent<HTMLButtonElement, MouseEvent>
 
   test('should initialize with the correct view mode and current period', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'month' }));
-    expect(result.current.viewMode).toBe('month');
-    expect(result.current.currPeriod).toBe(Temporal.Now.plainDateISO().toString({ calendarName: 'auto' }).substring(0, 7));
-  });
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'month' }),
+    )
+    expect(result.current.viewMode).toBe('month')
+    expect(result.current.currPeriod).toBe(
+      Temporal.Now.plainDateISO()
+        .toString({ calendarName: 'auto' })
+    )
+  })
 
   test('should navigate to the previous period correctly', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'month' }));
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'month' }),
+    )
 
     act(() => {
-      result.current.getPrev(mockEvent);
-    });
+      result.current.getPrev(mockEvent)
+    })
 
-    const expectedPreviousMonth = Temporal.Now.plainDateISO().subtract({ months: 1 });
-    expect(result.current.currPeriod).toBe(expectedPreviousMonth.toString({ calendarName: 'auto' }).substring(0, 7));
-  });
+    const expectedPreviousMonth = Temporal.Now.plainDateISO().subtract({
+      months: 1,
+    })
+    expect(result.current.currPeriod).toBe(
+      expectedPreviousMonth.toString({ calendarName: 'auto' }),
+    )
+  })
 
   test('should navigate to the next period correctly', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'month' }));
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'month' }),
+    )
 
     act(() => {
-      result.current.getNext(mockEvent);
-    });
+      result.current.getNext(mockEvent)
+    })
 
-    const expectedNextMonth = Temporal.Now.plainDateISO().add({ months: 1 });
-    expect(result.current.currPeriod).toBe(expectedNextMonth.toString({ calendarName: 'auto' }).substring(0, 7));
-  });
+    const expectedNextMonth = Temporal.Now.plainDateISO().add({ months: 1 })
+    expect(result.current.currPeriod).toBe(
+      expectedNextMonth.toString({ calendarName: 'auto' }),
+    )
+  })
 
   test('should reset to the current period correctly', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'month' }));
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'month' }),
+    )
 
     act(() => {
-      result.current.getNext(mockEvent);
-      result.current.getCurrent(mockEvent);
-    });
+      result.current.getNext(mockEvent)
+      result.current.getCurrent(mockEvent)
+    })
 
-    expect(result.current.currPeriod).toBe(Temporal.Now.plainDateISO().toString({ calendarName: 'auto' }).substring(0, 7));
-  });
+    expect(result.current.currPeriod).toBe(
+      Temporal.Now.plainDateISO().toString({ calendarName: 'auto' })
+    )
+  })
 
   test('should change view mode correctly', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'month' }));
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'month' }),
+    )
 
     act(() => {
-      result.current.changeViewMode('week');
-    });
+      result.current.changeViewMode('week')
+    })
 
-    expect(result.current.viewMode).toBe('week');
-  });
+    expect(result.current.viewMode).toBe('week')
+  })
 
   test('should select a day correctly', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'month' }));
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'month' }),
+    )
 
     act(() => {
-      result.current.get(Temporal.PlainDate.from('2024-06-01'));
-    });
+      result.current.get(Temporal.PlainDate.from('2024-06-01'))
+    })
 
-    expect(result.current.currPeriod).toBe('2024-06-01');
-  });
+    expect(result.current.currPeriod).toBe('2024-06-01')
+  })
 
   test('should return the correct props for an event', () => {
-    const { result } = renderHook(() => useCalendar({ events, viewMode: 'week' }));
+    const { result } = renderHook(() =>
+      useCalendar({ events, viewMode: 'week' }),
+    )
 
-    const eventProps = result.current.getEventProps('1');
+    const eventProps = result.current.getEventProps('1')
 
     expect(eventProps).toEqual({
       style: {
         position: 'absolute',
-        top: '41.66666666666667%',
+        top: 'min(41.66666666666667%, calc(100% - 55px))',
         left: '2%',
         width: '96%',
         margin: 0,
-        height: '5.555555555555555%',
+        height: '8.333333333333332%',
       },
-    });
-  });
-});
+    })
+  })
+})
