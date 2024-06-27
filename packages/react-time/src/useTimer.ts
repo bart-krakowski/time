f23fb69ed3c0b4963fc89bb5ee9aa83b6dc34a20
Bart Krakowski <bartlomiej@krakowski.dev>
2024-06-27 23:01:52 +0200

    feat: useTimer

---
+++ packages/react-time/src/useTimer.ts
@@ -0,0 +1,23 @@
+import { useStore } from '@tanstack/react-store'
+import { Timer, type TimerApi, type TimerOptions } from '@tanstack/time'
+import { useCallback, useState } from 'react'
+
+
+export const useTimer = (options: TimerOptions): TimerApi => {
+  const [timer] = useState(() => new Timer(options))
+  const state = useStore(timer.store)
+
+  const start = useCallback<typeof timer.start>(() => {
+    timer.start()
+  }, [timer])
+
+  const stop = useCallback<typeof timer.stop>(() => {
+    timer.stop()
+  }, [timer])
+
+  const reset = useCallback<typeof timer.reset>(() => {
+    timer.reset()
+  }, [timer])
+
+  return { ...state, start, stop, reset }
+}
