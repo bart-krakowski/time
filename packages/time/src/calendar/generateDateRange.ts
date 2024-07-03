20fb06d40037d8f3ea95f771f98f67f0963131f2
Bart Krakowski <bartlomiej@krakowski.dev>
2024-07-04 00:06:51 +0200

    refactor: Update generateDateRange to accept string inputs

--- packages/time/src/calendar/generateDateRange.ts
+++ packages/time/src/calendar/generateDateRange.ts
@@ -1,12 +1,18 @@
 import { Temporal } from '@js-temporal/polyfill'
+import { validateDate } from '../utils/validateDate'
 
 export const generateDateRange = (
-  start: Temporal.PlainDate,
-  end: Temporal.PlainDate,
+  start: string,
+  end: string,
 ): Temporal.PlainDate[] => {
+  validateDate({ date: start })
+  validateDate({ date: end })
+
+  const startDate = Temporal.PlainDate.from(start)
+  const endDate = Temporal.PlainDate.from(end)
   const dates: Temporal.PlainDate[] = []
-  let current = start
-  while (Temporal.PlainDate.compare(current, end) <= 0) {
+  let current = startDate
+  while (Temporal.PlainDate.compare(current, endDate) <= 0) {
     dates.push(current)
     current = current.add({ days: 1 })
   }
